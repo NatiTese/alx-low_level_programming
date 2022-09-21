15e39fb24020080e3ea5eb824f784c91572da2b8
Nati Tese <Nathytese@gmail.com>
2022-09-21 10:51:36 -0700

    fixed a stupid betty error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,59 +11,35 @@ void jack_bauer(void)
 
 	int a = 0, b = 0, c = 0, d = 0;
 
-
-
 	while (x < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
 
-				{
+		d++;
 
-							_putchar(a + '0');
+		if (d > 9)
+		{
+			d = 0;
 
-									_putchar(b + '0');
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
 
-											_putchar(':');
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
 
-													_putchar(c + '0');
-
-															_putchar(d + '0');
-
-																	_putchar('\n');
-
-
-
-																			d++;
-
-																					if (d > 9)
-
-																								{
-
-																												d = 0;
-
-																															c++;
-
-																																	}
-
-																							if (c > 5)
-
-																										{
-
-																														c = 0;
-
-																																	b++;
-
-																																			}
-
-																									if (b > 9)
-
-																												{
-
-																																b = 0;
-
-																																			a++;
-
-																																					}
-
-																											x++;
-
-																												}
+			a++;
+		}
+		x++;
+	}
 }
